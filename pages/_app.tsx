70047b9d46a72c42823140f49310e0b198ad56cc
Júlio Faria <juliofariaa@gmail.com>
2021-06-03 16:44:58 -0300

    Tela inicial criada

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import "@fontsource/roboto";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
